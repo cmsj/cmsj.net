6de7627e0f0c2c377b8ddf7c27f7a35a7b21e1bf
Chris Jones <cmsj@tenshu.net>
2018-09-20 09:21:01 +0100

    Switch from using IPNS to a Route53 command

--- update_ipfs.sh
+++ update_ipfs.sh
@@ -8,5 +8,13 @@ rsync -qrx --stats output/ gnubert.local:/srv/tank/docker/ipfs/staging/cmsj.net
 echo "Publishing files to IPFS..."
 HASH=$(ssh gnubert.local docker exec ipfs ipfs add -Q -r /export/cmsj.net | tr -d '\r')
 
-echo "Linking files to cmsj.net..."
-ssh gnubert.local docker exec ipfs ipfs name publish --key=cmsj.net "${HASH}"
+echo "Pushing new hash to Route53..."
+HOSTEDZONE=ZLC6XW8O2UH0
+NAME=ipfs.cmsj.net
+REQUEST='{"HostedZoneId":"'$HOSTEDZONE'","ChangeBatch":{"Comment":"Updating '$NAME' to '$HASH'","Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"_dnslink.'$NAME'","Type":"TXT","TTL":30,"ResourceRecords":[{"Value":"\"dnslink=/ipfs/'$HASH'\""}]}}]}}'
+
+source ../r53_env.txt
+aws route53 change-resource-record-sets --cli-input-json "${REQUEST}"
+
+#echo "Linking files to cmsj.net..."
+#ssh gnubert.local docker exec ipfs ipfs name publish --key=cmsj.net "${HASH}"
